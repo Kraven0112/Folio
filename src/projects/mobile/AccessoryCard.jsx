e9ccee2289b360f21f654db32096e7d6a9295291
Kraven0112 <sushilChaudhary2059@gmail.com>
2024-10-11 16:26:26 +0530

    c

--- src/projects/mobile/AccessoryCard.jsx
+++ src/projects/mobile/AccessoryCard.jsx
@@ -5,7 +5,7 @@ import { ThemeContext } from '../../contexts/ThemeContext'
 export default function AccessoryCard({image,title}) {
     const[isDark] = useContext(ThemeContext)
   return (
-    <Link to={`/accessorydetail/${title}`} className={`block text-center rounded-2xl w-[320px] h-[340px] sm:w-[340px] sm:h-[410px] sm:p-1 ${isDark?"shadow-sm shadow-white":"shadow-lg"}`}>
+    <Link to={`/accessorydetail/${title}`} className={`block text-center rounded-2xl w-[full] h-[340px] sm:w-[340px] sm:h-[410px] sm:p-1 ${isDark?"shadow-sm shadow-white":"shadow-lg"}`}>
       <img className=' w-full h-[85%] sm:w-full sm:h-[90%]' src={image} alt="accessory" />
       <h1 className='sm:mt-1 sm:text-2xl text-xl font-bold'>{title}</h1>
     </Link>
